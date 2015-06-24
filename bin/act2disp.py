9fec79a52bace1b01b6ad3b5df3d8dbc13884529
Vasiliy <vkraskov@gmail.com>
2015-06-24 17:25:57 +0300

    v1.0

---
+++ bin/act2disp.py
@@ -0,0 +1,327 @@
+# NeoPixel library strandtest example
+# Author: Tony DiCola (tony@tonydicola.com)
+#
+# Direct port of the Arduino NeoPixel library strandtest example.  Showcases
+# various animations on a strip of NeoPixels.
+import time
+import math
+import thread
+import threading
+#from colour import Color as Colour
+from threading import Thread
+import os
+import mpeasing
+
+from neopixel import *
+
+
+# LED strip configuration:
+LED_COUNT      = 16      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 64      # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+
+####
+
+
+class DisplayThread (threading.Thread):
+    def __init__(self, threadID, name, strip, pixels):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.strip = strip
+        self.pixels = pixels
+        self.pixels_prev = [Color(0,0,0) for i in range(16)]
+
+    def run(self):
+        print "Starting " + self.name
+	#
+	while not time2quit:
+        	self.loop()
+	#
+        print "Exiting " + self.name
+	thread.exit()
+
+    def loop(self):
+	changed = False
+#	print "looping.."
+	for i in range(16):
+		if self.pixels_prev[i] <> self.pixels[i]:
+			changed = True
+#			print "changed = True"
+			break
+	if changed: 
+		display_lock.acquire()
+		for i in range(16):
+			strip.setPixelColor(i, self.pixels[i])
+#		print "showing.."
+		strip.show()
+		display_lock.release()
+		for i in range(16):
+			self.pixels_prev[i] = self.pixels[i]
+	time.sleep(100/1000.0)
+#
+
+class SpriteThread (threading.Thread):
+    def __init__(self, threadID, name, sprite, pixels, delayMove, delayShine):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.sprite = []
+	for i in range(16):
+		self.sprite.append(sprite[i])
+        self.pixels = pixels
+        self.delayMove = delayMove
+        self.delayShine = delayShine
+        self.changed = False
+       # self.composeThread = null
+        self.brights = [0 for i in range(16)]
+	self.spriteLock = threading.Lock()
+
+    def run(self):
+        print "Starting " + self.name
+	#
+	threadMove = threading.Thread(target=self.move, args=(self.delayMove,))
+	threadMove.start()
+	threadShine = threading.Thread(target=self.shine, args=(self.delayShine,))
+	threadShine.start()
+	while threadMove.isAlive() or threadShine.isAlive():
+		time.sleep(100/1000.0)
+		
+	#
+	self.sprite[:] = []
+        print "Exiting " + self.name
+	thread.exit()
+
+    def move(self, delay_ms):
+	for k in range(32):
+#		print "moving " + str(k)
+		self.spriteLock.acquire()
+#		print "Sprite = " + str(self.sprite[0]) + " " + str(self.sprite[1]) + " " + str(self.sprite[2]) + " " + str(self.sprite[3]) + " " + str(self.sprite[4]) + " " + str(self.sprite[5]) + " " + str(self.sprite[6]) + " " + str(self.sprite[7]) + " " + str(self.sprite[8])
+		pix15 = self.sprite[15]
+		for i in range(15, 0, -1):
+			self.sprite [i] = self.sprite[i-1]
+		self.sprite[0] = pix15
+#		print "Sprite = " + str(self.sprite[0]) + " " + str(self.sprite[1]) + " " + str(self.sprite[2])
+		self.changed = True
+		self.spriteLock.release()
+		#
+		time.sleep(delay_ms/1000.0)
+	print "move done"
+
+    def shine(self, delay_ms):
+	for k in range(0, 251, 50):
+		self.spriteLock.acquire()
+		for i in range(16):
+			self.brights[i] = k
+		self.changed = True
+		self.spriteLock.release()
+		time.sleep(delay_ms/1000.0)
+	#
+	for k in range(251, 0, -10):
+		self.spriteLock.acquire()
+		for i in range(16):
+			self.brights[i] = k
+		self.changed = True
+		self.spriteLock.release()
+		time.sleep(delay_ms/1000.0)
+	print "shine done"
+
+#
+class StillSpriteThread (SpriteThread):
+    def __init__(self, threadID, name, sprite, pixels, delayShine, secShine):
+        SpriteThread.__init__(self, threadID, name, sprite, pixels, 0, delayShine)
+	self.secShine = secShine
+
+
+    def move(self, delay_ms):
+	a=1
+
+    def shine(self, delay_ms):
+	for k in range(0, 251, 50):
+		self.spriteLock.acquire()
+		for i in range(16):
+			self.brights[i] = k
+		self.changed = True
+		self.spriteLock.release()
+		time.sleep(delay_ms/1000.0)
+	#
+	time.sleep(self.secShine)
+	#
+	for k in range(251, 0, -10):
+		self.spriteLock.acquire()
+		for i in range(16):
+			self.brights[i] = k
+		self.changed = True
+		self.spriteLock.release()
+		time.sleep(delay_ms/1000.0)
+	print "shine done"
+
+
+#
+
+class ComposeThread (threading.Thread):
+    def __init__(self, threadID, name, pixels):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.pixels = pixels
+
+    def run(self):
+        print "Starting " + self.name
+	#
+	while not time2quit:
+        	self.loop()
+	#
+        print "Exiting " + self.name
+	thread.exit()
+
+    def loop(self):
+	display_lock.acquire()
+	for i in range(strip.numPixels()):
+		pixels[i] = Color(0, 0, 0)
+	compose_lock.acquire()
+	for k in range(len(sprites)):
+		if sprites[k].isAlive():
+		#	print "Alive: " + sprites[k].name
+			sprites[k].spriteLock.acquire()
+			for i in range(strip.numPixels()):
+				p1_r = ( pixels[i] >> 16 ) & 0xFF;
+				p1_g = ( pixels[i] >>  8 ) & 0xFF;
+				p1_b = ( pixels[i]       ) & 0xFF;
+				p2_r = ( sprites[k].sprite[i] >> 16 ) & 0xFF;
+				p2_g = ( sprites[k].sprite[i] >>  8 ) & 0xFF;
+				p2_b = ( sprites[k].sprite[i]       ) & 0xFF;
+				p2_r = p2_r * sprites[k].brights[i]/256;
+				p2_g = p2_g * sprites[k].brights[i]/256;
+				p2_b = p2_b * sprites[k].brights[i]/256;
+				if (p1_r > 0 and p2_r > 0):
+					p1_r = ( (p1_r + p2_r)/2 ) & 0xFF;
+				else:
+					p1_r = ( p1_r + p2_r ) & 0xFF;
+				if (p1_g > 0 and p2_g > 0):
+					p1_g = ( (p1_g + p2_g)/2 ) & 0xFF;
+				else:
+					p1_g = ( p1_g + p2_g ) & 0xFF;
+				if (p1_b > 0 and p2_b > 0):
+					p1_b = ( (p1_b + p2_b)/2 ) & 0xFF;
+				else:
+					p1_b = ( p1_b + p2_b ) & 0xFF;
+				pixels[i] = Color(p1_r, p1_g, p1_b)
+			sprites[k].changed = False
+			sprites[k].spriteLock.release()
+	compose_lock.release()
+	display_lock.release()
+	time.sleep(100/1000.0)
+
+
+
+def clearStrip():
+	for i in range(strip.numPixels()):
+		strip.setPixelColor(i, Color(0, 0, 0))
+	strip.show()
+
+
+# Main program logic follows:
+if __name__ == '__main__':
+
+	hub_home=os.environ.get('HUB_HOME', '/opt/monpanel.com/hub')
+
+	# Create NeoPixel object with appropriate configuration.
+	strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+	# Intialize the library (must be called once before other functions).
+	strip.begin()
+	clearStrip()
+
+
+	pixels=[Color(0,0,0) for i in range(16)]
+
+	signal1Sprite=[Color(0,0,0) for i in range(16)]
+	signal1Sprite[0]=Color(0,0,16)
+	signal1Sprite[1]=Color(0,0,32)
+	signal1Sprite[2]=Color(0,0,64)
+
+	signal2Sprite=[Color(0,0,0) for i in range(16)]
+	signal2Sprite[0]=Color(0,16,0)
+	signal2Sprite[1]=Color(0,32,0)
+	signal2Sprite[2]=Color(0,64,0)
+
+	signal3Sprite=[Color(0,0,0) for i in range(16)]
+	signal3Sprite[0]=Color(64,32,32)
+	signal3Sprite[1]=Color(64,32,32)
+	signal3Sprite[2]=Color(64,32,32)
+
+	signal4Sprite=[Color(0,0,0) for i in range(16)]
+	signal4Sprite[3]=Color(32,32,64)
+	signal4Sprite[4]=Color(32,32,64)
+	signal4Sprite[5]=Color(32,32,64)
+
+	signal5Sprite=[Color(0,0,0) for i in range(16)]
+	signal5Sprite[6]=Color(32,64,32)
+	signal5Sprite[7]=Color(32,64,32)
+	signal5Sprite[8]=Color(32,64,32)
+
+	sprites=[]
+
+	display_lock = threading.Lock()
+	compose_lock = threading.Lock()
+	time2quit = False
+
+	try:
+		displayThread = DisplayThread(1, "displayThread", strip, pixels)
+		displayThread.start()
+
+		composeThread = ComposeThread(2, "composeThread", pixels)
+		composeThread.start()
+
+	except Exception as e:
+		print "Error: unable to start thread"
+		print str(e)
+
+	try:
+		time.sleep(100/1000.0)
+
+		while True:
+			if os.path.exists(hub_home + "/tmp/pulldata_signal.dat"): 
+				os.remove(hub_home + "/tmp/pulldata_signal.dat")
+				th = SpriteThread(4, "signal2SpriteThread", signal2Sprite, pixels, 100, 50)
+				sprites.append(th)
+				th.start()
+				#
+			if os.path.exists(hub_home + "/tmp/pulldata_start.dat"): 
+				os.remove(hub_home + "/tmp/pulldata_start.dat")
+				th = StillSpriteThread(5, "signal3SpriteThread", signal3Sprite, pixels, 25, 5)
+				sprites.append(th)
+				th.start()
+				#
+			if os.path.exists(hub_home + "/tmp/pushdata_start.dat"): 
+				os.remove(hub_home + "/tmp/pushdata_start.dat")
+				th = StillSpriteThread(6, "signal4SpriteThread", signal4Sprite, pixels, 25, 5)
+				sprites.append(th)
+				th.start()
+				#
+			if os.path.exists(hub_home + "/tmp/rttmon_start.dat"): 
+				os.remove(hub_home + "/tmp/rttmon_start.dat")
+				th = StillSpriteThread(7, "signal5SpriteThread", signal5Sprite, pixels, 25, 5)
+				sprites.append(th)
+				th.start()
+				#
+			time.sleep(500/1000.0)
+
+	except KeyboardInterrupt:
+		print("Ok ok, quitting")
+	finally:
+		time2quit = True;
+		displayThread.join()
+		composeThread.join()
+		for i in range(len(sprites)):
+			if sprites[i].isAlive():
+				sprites[i].join()
+		clearStrip()
+		time.sleep(100/1000.0)
+
+
+
+
---
+++ bin/pulldata.py
@@ -0,0 +1,120 @@
+#
+#http://blog.oscarliang.net/raspberry-pi-and-arduino-connected-serial-gpio/
+
+import os
+import sqlite3 as lite 
+import serial
+import re
+import time
+import RPi.GPIO as GPIO 
+#import crc16
+from monutils import neodisplay
+
+hub_home=os.environ.get('HUB_HOME', '/opt/monpanel.com/hub')
+#hub_home = '/opt/monpanel.com/hub'
+
+conn = None
+conn = lite.connect(hub_home + '/dat/local.db') 
+cur = conn.cursor() 
+
+ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
+ser.open()
+
+flog=open(hub_home + '/log/pulldata.log', 'w+', 0)
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+def resetradio():
+	GPIO.setup(7, GPIO.OUT)
+	GPIO.output(7, GPIO.LOW)
+	time.sleep(0.1)
+	GPIO.output(7, GPIO.HIGH)
+	time.sleep(0.1)
+
+# http://www.lammertbies.nl/comm/info/crc-calculation.html
+#
+def crc16(buff, crc = 0, poly = 0xa001):
+    l = len(buff)
+    i = 0
+    while i < l:
+        ch = ord(buff[i])
+        uc = 0
+        while uc < 8:
+            if (crc & 1) ^ (ch & 1):
+                crc = (crc >> 1) ^ poly
+            else:
+                crc >>= 1
+            ch >>= 1
+            uc += 1
+        i += 1
+    return crc
+
+def LOG(str):
+	flog.write(str);
+	flog.write("\n");
+	flog.flush();
+
+#def neodisplay(name, data):
+#	try:
+#		f = open(hub_home + '/tmp/' + name + '.dat','w')
+#		f.truncate()
+#		f.write(data + '\n')
+#	except Exception as e:
+#		LOG(str(e))
+#	finally:
+#		f.close()
+#
+
+
+#ser.write("testing")
+
+neodisplay('pulldata_start', 'dummy')
+
+resetradio()
+lt1 = time.time()
+try:
+        while 1:
+		lt2 = time.time()
+		# if no data within 5 min, then reset atmega
+		if lt2-lt1 > 300:
+			resetradio()
+			time.sleep(1)
+			lt1 = time.time()
+
+                response = ser.readline()
+		#response = "DATA#TU12-9K78:t=25:h=27:v=3.3:c=19:10719#\n"
+                if not (response == ""):
+                        data = response.rstrip('\n')
+			LOG(data);
+			if (0 != data.find("DATA#")): continue
+			data = data[5:-2]
+			#data = data[:-1]
+			#LOG(data)
+			data_wo_crc = data[0:data.rfind(':')]
+			#LOG("DATA_WO_CRC = " + data_wo_crc)
+			arr = re.split(':',data)
+			crc =  arr[len(arr)-1]
+			#LOG("CRC16=" + crc)
+			#crc_calc = crc16.calcString(data_wo_crc, 0) 
+			crc_calc = crc16(data_wo_crc)
+			#print "CRC16_CALC=" + str(crc_calc)
+			if int(crc) == crc_calc: 
+				lt1 = time.time()
+				cur.execute("INSERT INTO queue (dat_str) VALUES ('%s')" % data_wo_crc)
+				conn.commit()
+                        	LOG("inserted row")
+
+				neodisplay('pulldata_signal', data_wo_crc)
+
+			else:
+                        	LOG("crc doesnt match")
+				
+except KeyboardInterrupt:
+	LOG("KeyboardInterrupt")
+finally:
+        ser.close()
+	conn.close();
+	flog.close();
+	GPIO.cleanup()
+
